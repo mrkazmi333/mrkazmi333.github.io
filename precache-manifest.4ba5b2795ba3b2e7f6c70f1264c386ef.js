5affa8efd30151517b2290ef0690967ab3223694
mrkcancode <motasimkazmi333@gmail.com>
2021-02-15 20:15:31 +0530

    Updates

---
+++ precache-manifest.4ba5b2795ba3b2e7f6c70f1264c386ef.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8bd406b208a894d8c30758240c5e044",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a28f86b7725837a2b5b7",
+    "url": "/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "8f5beac6b1341710c09f",
+    "url": "/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "c445a5b6b73b65b1b08b",
+    "url": "/static/css/main.4a1a03d7.chunk.css"
+  },
+  {
+    "revision": "7561d104032e2324308e",
+    "url": "/static/js/2.1be86424.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.1be86424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28f86b7725837a2b5b7",
+    "url": "/static/js/3.e63b2a01.chunk.js"
+  },
+  {
+    "revision": "8f5beac6b1341710c09f",
+    "url": "/static/js/4.2f49edbd.chunk.js"
+  },
+  {
+    "revision": "c445a5b6b73b65b1b08b",
+    "url": "/static/js/main.ef291c28.chunk.js"
+  },
+  {
+    "revision": "de3f8db4f8c5410433ef",
+    "url": "/static/js/runtime-main.516ddbfe.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "621d97a83e3355e2544bd8cba2341aed",
+    "url": "/static/media/intelSoftwareInnovatorLogo.621d97a8.webp"
+  },
+  {
+    "revision": "bd4c51da740cfef96771b1b3ba155a09",
+    "url": "/static/media/jamiaHamdardLogo.bd4c51da.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "96e4d4bfce5109433ad725f464fc7f64",
+    "url": "/static/media/orientalCollegeLogo.96e4d4bf.png"
+  },
+  {
+    "revision": "d2e542b0015c443070967c9e291339cf",
+    "url": "/static/media/pyDataGlobalLogo.d2e542b0.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  }
+]);
